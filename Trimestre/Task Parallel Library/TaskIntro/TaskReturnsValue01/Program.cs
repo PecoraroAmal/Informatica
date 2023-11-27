200109cc74ea4e47116b90767128d47088073f18
Pecoraro Amal <amal.pecoraro@issgreppi.it>
2023-11-27 08:51:42 +0100

    Lezione del 27 Novembre

---
+++ Trimestre/Task Parallel Library/TaskIntro/TaskReturnsValue01/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace TaskReturnsValue01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //qui int è il tipo restituito dal Task
+            //si noti che in questo caso si è usato il costrutto Task.Run(lambda) e non new Task(lambda)
+            Task<int> t = Task.Run(() =>
+            {
+                return 32;
+            });
+            //qui leggiamo il valore restituito dal Task
+            //è come fare una Join sul thread del Task oppure come invocare la Wait sul Task
+            Console.WriteLine(t.Result);
+            Console.WriteLine();
+            Console.WriteLine("Press Enter to terminate!");
+            Console.ReadLine();
+        }
+    }
+}
