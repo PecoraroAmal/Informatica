da3174c15ee7230f007557685636927d3e0c93c2
Pecoraro Amal <amal.pecoraro@issgreppi.it>
2023-11-30 09:02:04 +0100

    Lezione del 30-11-2023

---
+++ Trimestre/Calcolo parallelo vs Calcolo sequenziale/ParallelForDemo/ParallelForEachDemo/Program.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Concurrent;
+using System.Diagnostics;
+
+namespace ParallelForEachDemo
+{
+    class Program
+    {
+        //https://learn.microsoft.com/en-us/dotnet/standard/parallel-programming/how-to-write-a-simpleparallel-foreach-loop
+        static void Main()
+        {
+            // 2 million
+            var limit = 30_000_000;//l'underscore non inficia sul programma, serve per vedere meglio le cifre
+            var numbers = Enumerable.Range(0, limit).ToList();//creo una lista con i valori da 0 a limit
+            var watch = Stopwatch.StartNew();
+            var primeNumbersFromForeach = GetPrimeList(numbers);//restituisco la lista con i numeri primi
+            watch.Stop();
+            var watchForParallel = Stopwatch.StartNew();
+            var primeNumbersFromParallelForeach = GetPrimeListWithParallel(numbers);
+            //restituisco la lista con i numeri primi con il calcolo parallelo
+            watchForParallel.Stop();
+            Console.WriteLine($"Classical Foreach loop | Total prime numbers : {primeNumbersFromForeach.Count} | Time Taken: {watch.ElapsedMilliseconds} ms.");
+            Console.WriteLine($"Parallel  ForEach loop | Total prime numbers : {primeNumbersFromParallelForeach.Count} | Time Taken: {watchForParallel.ElapsedMilliseconds} ms.");
+            //calcoliamo il fattore di speedup
+            double speedup = (double)(watch.ElapsedMilliseconds) / watchForParallel.ElapsedMilliseconds;
+            Console.WriteLine($"Il fattore di speedup è {speedup:F2}. Dunque Il calcolo parallelo è {speedup:F2} volte più veloce di quello sequenziale");
+        }
+        /// <summary>
+        /// GetPrimeList returns Prime numbers by using sequential ForEach
+        /// </summary>
+        /// <param name="inputs"></param>
+        /// <returns></returns>
+        private static IList<int> GetPrimeList(IList<int> numbers) => numbers.Where(IsPrime).ToList();
+        //filtro i numeri contenuti nella lista in base al metodo/predicatoIsPrime
+        /// <summary>
+        /// GetPrimeListWithParallel returns Prime numbers by using Parallel.ForEach
+        /// </summary>
+        /// <param name="numbers"></param>
+        /// <returns></returns>
+        private static IList<int> GetPrimeListWithParallel(IList<int> numbers)
+        {
+            //https://learn.microsoft.com/en-us/dotnet/api/system.collections.concurrent.concurrentbag1?view=net-7.0
+            //Represents a thread-safe, unordered collection of objects.
+            var primeNumbers = new ConcurrentBag<int>();
+            Parallel.ForEach(numbers, number =>
+            {
+                if (IsPrime(number))
+                {
+                    primeNumbers.Add(number);
+                }
+            });
+            return primeNumbers.ToList();
+        }
+        /// <summary>
+        /// IsPrime returns true if number is Prime, else false.(https://en.wikipedia.org/wiki/Prime_number)
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns></returns>
+        private static bool IsPrime(int number)
+        {
+            if (number <= 2)
+            {
+                return false;
+            }
+            for (var divisor = 2; divisor <= Math.Sqrt(number); divisor++)
+            {
+                if (number % divisor == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+}
---
+++ Trimestre/Calcolo parallelo vs Calcolo sequenziale/ParallelForDemo/ParallelForWithThreadLocalVariabiles/Program.cs
@@ -0,0 +1,78 @@
+﻿using System.Diagnostics;
+
+namespace ParallelForWithThreadLocalVariabiles
+{
+    internal class Program
+    {
+        static readonly object _lock = new object();
+        static void Main(string[] args)
+        {
+            const int Numbers = 1000000;
+            int[] nums = Enumerable.Range(1, Numbers).ToArray();//creo un array con i valori da 1 a N
+            long total = 0;
+            var watch = Stopwatch.StartNew();
+            //Formula di Gauss con il Parallel.For non ottimizzata
+            Parallel.For(0, Numbers, i =>
+            {
+                lock (_lock)
+                {
+                    total += nums[i];
+                }
+            });
+            watch.Stop();
+            var millisecondiLock = watch.ElapsedMilliseconds;
+            Console.WriteLine($"La somma dei primi {Numbers} è: {total} e con il lock impiego: {millisecondiLock}ms");
+            total = 0;
+            watch.Restart();
+            for(int i = 0; i < Numbers; i++)
+            {
+                total += nums[i];
+            }
+            watch.Stop();
+            var millisecondiSequenziale = watch.ElapsedMilliseconds;
+            Console.WriteLine($"La somma dei primi {Numbers} è: {total} e con il calcolo sequenziale impiego: {millisecondiSequenziale}ms");
+            double speed = (double)millisecondiLock / millisecondiSequenziale;
+            Console.WriteLine($"Speed: {speed}");
+            total = 0;
+            watch.Restart();
+            Parallel.For(0, Numbers, i =>
+            {
+                Interlocked.Add(ref total, nums[i]);
+            });
+            millisecondiLock = watch.ElapsedMilliseconds;
+            Console.WriteLine($"La somma dei primi {Numbers} è: {total} e con Interlocked impiego: {millisecondiLock}ms");
+            speed = (double)millisecondiLock / millisecondiSequenziale;
+            Console.WriteLine($"Speed: {speed}");
+            //https://learn.microsoft.com/enus/dotnet/api/system.threading.tasks.parallel.for?view=net-7.0
+            //https://learn.microsoft.com/enus/dotnet/api/system.threading.tasks.parallel.for?f1url=%3FappId%3DDev16IDEF1%26l%3DENUS%26k%3Dk(System.Threading.Tasks.Parallel.For%2560%25601)%3Bk(DevLang-csharp)%26rd%3Dtrue&view=net7.0#system-threading-tasks-parallel-for-1(system-int32-system-int32-system-threading-tasksparalleloptions-system-func((-0))-system-func((system-int32-system-threading-tasks-parallelloopstate0-0))-system-action((-0)))
+            // Use type parameter to make subtotal a long, not an int
+            //()=>0 inizializza la variabile locale al Task, subtotal in questo caso. La variabile subtotal viene inizializzata a 0
+            total = 0;
+            watch.Restart();
+            Parallel.For<long>(0, nums.Length, () => 0, (j, loopState, subtotal) =>
+            {
+                subtotal += nums[j];//modify local variable
+                return subtotal;
+            },
+            // value to be passed to next iteration
+            //la variabile x è il risultato dell'elaborazione del Task. Corrisponde a subtotal quando il task finisce la sua elaborazione
+            //Questa è la action che viene eseguita alla fine dell'elaborazione del Task
+            (x) => Interlocked.Add(ref total, x));
+            watch.Stop();
+            var millisecondiOptimizate = watch.ElapsedMilliseconds;
+            Console.WriteLine($"La somma dei primi {Numbers} è: {total} e con il lock impiego: {millisecondiOptimizate}ms");
+            speed = (double)millisecondiOptimizate / millisecondiSequenziale;
+            Console.WriteLine($"Speed: {speed}");
+
+            //confrontiamo il risultato con la formula di Gauss
+            total = 0;
+            watch.Restart();
+            long sommaGauss = (long)Numbers * (Numbers + 1) / 2;
+            watch.Stop();
+            var millisecondiGauss = watch.ElapsedMilliseconds;
+            Console.WriteLine($"La somma dei primi {Numbers} è: {sommaGauss} e con il lock impiego: {millisecondiGauss}ms");
+            speed = (double)millisecondiGauss / millisecondiSequenziale;
+            Console.WriteLine($"Speed: {speed}");
+        }
+    }
+}
